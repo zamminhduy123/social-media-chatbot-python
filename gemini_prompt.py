42bb25269851a35e20c16a2eeafd6c0435ce161d
duotien <notduotien@gmail.com>
2025-04-21 18:48:13 +0700

    Add: prompt safety settings

--- gemini_prompt.py
+++ gemini_prompt.py
@@ -3,19 +3,20 @@ from google.genai import types
 
 
 SYSTEM_PROMPT = ""
-with open("system_prompt.txt","r", encoding="utf8") as fhandle:
+with open("system_prompt.txt", "r", encoding="utf8") as fhandle:
     SYSTEM_PROMPT = fhandle.read()
 
 MODEL_ID = "gemini-2.0-flash"
-TEMPERATURE=0
-TOP_P=0.95
-TOP_K=10
-CANDIDATE_COUNT=1
-SEED=5
+TEMPERATURE = 0
+TOP_P = 0.95
+TOP_K = 10
+CANDIDATE_COUNT = 1
+SEED = 5
 
 PLACEHOLDER = """Chào bạn! Mình là chatbot hỗ trợ của KNI Education, rất vui
 được hỗ trợ bạn. Bạn có câu hỏi nào về luyện thi TestAS hay tư vấn du học Đức
 không? Mình sẽ cố gắng trả lời một cách chi tiết và đầy đủ nhất có thể! 😊"""
+
 
 # === GenerateContentConfig ===
 def get_chat_config():
@@ -26,4 +27,22 @@ def get_chat_config():
         top_k=TOP_K,
         candidate_count=CANDIDATE_COUNT,
         seed=SEED,
+        safety_settings=[
+            types.SafetySetting(
+                category="HARM_CATEGORY_HARASSMENT",
+                threshold="BLOCK_LOW_AND_ABOVE",  # Block most
+            ),
+            types.SafetySetting(
+                category="HARM_CATEGORY_HATE_SPEECH",
+                threshold="BLOCK_LOW_AND_ABOVE",  # Block most
+            ),
+            types.SafetySetting(
+                category="HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                threshold="BLOCK_LOW_AND_ABOVE",  # Block most
+            ),
+            types.SafetySetting(
+                category="HARM_CATEGORY_DANGEROUS_CONTENT",
+                threshold="BLOCK_LOW_AND_ABOVE",  # Block most
+            ),
+        ],
     )

e62710b0962760d7e57448f76c047738782e4860
Rzy <ntminhduy123@gmail.com>
2025-04-25 23:40:42 +0900

    Add: check msg source

--- app.py
+++ app.py
@@ -107,10 +107,8 @@ def webhook():
         data = request.get_json()
         for entry in data.get("entry", []):
             for message_event in entry.get("messaging", []):
-                messaging_platform = "facebook"
-                if "instagram" in entry.get("id", "").lower():
-                    messaging_platform = "instagram"
-                print("[Webhook]: Received message from", messaging_platform)
+                object_type = data.get("object", "")
+                print("[Webhook]: Received message from", object_type)
                 if "message" in message_event and "text" in message_event["message"]:
                     handle_user_message(message_event)
         return "ok", 200
